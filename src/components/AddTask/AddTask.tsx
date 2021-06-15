48e42e7be7b88574a61be24bcfc98fe1a4b9cdcc
Bazil Korath <korathbasil@live.com>
2021-06-15 22:13:06 +0530

    added backdrop blur , not working in firefox

---
+++ src/components/AddTask/AddTask.tsx
@@ -0,0 +1,11 @@
+import styles from "./AddTask.module.css";
+
+const AddTask: React.FC = () => {
+  return (
+    <div className={styles.addTask}>
+      <h3>aSDS</h3>
+    </div>
+  );
+};
+
+export default AddTask;
