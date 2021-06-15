e315cb3a6684a92dde3f9c1024d73ef7d182e393
Bazil Korath <korathbasil@live.com>
2021-06-15 21:44:36 +0530

    header added

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 
 import "./App.css";
+import Header from "./components/Header/Header";
 import List from "./components/List/List";
 
 function App() {
@@ -14,7 +15,9 @@ function App() {
   return (
     <div className="app">
       <div className="app-container">
-        <div className="app-left"></div>
+        <div className="app-left">
+          <Header />
+        </div>
         <div className="app-right"></div>
       </div>
     </div>
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,9 +1,11 @@
+import styles from "./Header.module.css";
 import Logo from "../../assets/logo.png";
 
 const Header = () => {
   return (
-    <div className="header">
-      <img src={Logo} alt="" className="logo" />
+    <div className={styles.header}>
+      <img src={Logo} alt="" className={styles.img} />
+      <h1>Tasker-X</h1>
     </div>
   );
 };
