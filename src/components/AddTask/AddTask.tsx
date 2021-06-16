cc52de7be6a430b3daa88748fda8ff9e268eb957
Bazil Korath <korathbasil@live.com>
2021-06-16 20:34:17 +0530

    background colorchange of addTaskForm works

--- src/components/AddTask/AddTask.tsx
+++ src/components/AddTask/AddTask.tsx
@@ -1,5 +1,7 @@
-import { useState, useRef, RefObject } from "react";
+import { useState, useRef } from "react";
 import styles from "./AddTask.module.css";
+
+import ClearIcon from "../../assets/clear-icon-outlined.png";
 
 interface addTaskProps {
   addTask: (task: string) => void;
@@ -7,6 +9,7 @@ interface addTaskProps {
 
 const AddTask: React.FC<addTaskProps> = ({ addTask }) => {
   const submitButttonRef = useRef<HTMLButtonElement>(null);
+  const addTaskRef = useRef<HTMLDivElement>(null);
   const [task, setTask] = useState("");
 
   const formSubmitHandler = (e: React.SyntheticEvent) => {
@@ -14,37 +17,49 @@ const AddTask: React.FC<addTaskProps> = ({ addTask }) => {
     addTask(task);
     setTask("");
   };
+  const form = document.querySelector(".addTask") as HTMLFormElement;
 
   return (
-    <form className={styles.addTask} onSubmit={formSubmitHandler}>
-      <div className={styles.inputContainer}>
-        <input
-          type="text"
-          value={task}
-          onChange={(e) => setTask(e.target.value)}
-          placeholder="Add task here..."
-        />
-      </div>
-      <div className={styles.colorPicker}>
-        <div className={styles.colorCircle}></div>
-        <div className={styles.colorCircle}></div>
-        <div className={styles.colorCircle}></div>
-        <div className={styles.colorCircle}></div>
-        <div className={styles.colorCircle}></div>
-        <div className={styles.colorCircle}></div>
-      </div>
-      <div className={styles.action}>
-        <button hidden type="submit" ref={submitButttonRef}>
-          Add
-        </button>
-        <div
-          className={styles.button}
-          onClick={() => submitButttonRef.current?.click()}
-        >
-          Add
+    <div className={styles.addTask} ref={addTaskRef}>
+      <form id="inputTaskForm" onSubmit={formSubmitHandler}>
+        <div className={styles.inputContainer}>
+          <input
+            type="text"
+            value={task}
+            onChange={(e) => {}}
+            placeholder="Add task here..."
+          />
+          <img src={ClearIcon} alt="" onClick={() => setTask("")} />
         </div>
-      </div>
-    </form>
+        <div className={styles.colorPicker}>
+          <div
+            className={styles.colorCircle}
+            onClick={() => {
+              if (addTaskRef.current !== null) {
+                addTaskRef.current.style.background =
+                  "linear-gradient(135deg, rgba(226,85,185,1) 0%, rgba(105,16,99,1) 100%)";
+              }
+            }}
+          ></div>
+          <div className={styles.colorCircle}></div>
+          <div className={styles.colorCircle}></div>
+          <div className={styles.colorCircle}></div>
+          <div className={styles.colorCircle}></div>
+          <div className={styles.colorCircle}></div>
+        </div>
+        <div className={styles.action}>
+          <button hidden type="submit" ref={submitButttonRef}>
+            Add
+          </button>
+          <div
+            className={styles.button}
+            onClick={() => submitButttonRef.current?.click()}
+          >
+            Add
+          </div>
+        </div>
+      </form>
+    </div>
   );
 };
 
